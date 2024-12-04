a785f547909e556d8f534379d3559caa0e2b71c3
chizkuhhh <chizkuhhh22@gmail.com>
2024-12-04 21:53:57 +0800

    Update db_config.py

--- stadvdb-mco2-app2/backend/app/db_config.py
+++ stadvdb-mco2-app2/backend/app/db_config.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
 db_config = {
-    "node1": {"host": "localhost", "port": 3306, "user": "replication_user", "password": "Yalowi7(", "database": "justgames"}, # change pw into ur own
-    "node2": {"host": "localhost", "port": 3307, "user": "replication_user", "password": "Yalowi7(", "database": "justgames"}, # change pw into ur own
-    "node3": {"host": "localhost", "port": 3308, "user": "replication_user", "password": "Yalowi7(", "database": "justgames"}  # change pw into ur own
+    "node1": {"host": "localhost", "port": 3306, "user": "replication_user", "password": " ", "database": "justgames"}, # change pw into ur own
+    "node2": {"host": "localhost", "port": 3307, "user": "replication_user", "password": " ", "database": "justgames"}, # change pw into ur own
+    "node3": {"host": "localhost", "port": 3308, "user": "replication_user", "password": " ", "database": "justgames"}  # change pw into ur own
 }
 
 def get_db_connection(node):
     return mysql.connector.connect(
         host=db_config[node]["host"],
-        port=db_config[node]["port"],  # Include the port
+        port=db_config[node]["port"],  
         user=db_config[node]["user"],
         password=db_config[node]["password"],
         database=db_config[node]["database"]
